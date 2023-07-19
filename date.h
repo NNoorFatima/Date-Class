149322d2723a052d4719eae4016c3e76196de1a0
NNoorFatima <50588372+NNoorFatima@users.noreply.github.com>
2023-07-19 08:22:39 +0300

    date.h
    has methods and member of the date class

---
+++ date.h
@@ -0,0 +1,25 @@
+//header file for date class
+class Date
+{
+    int day; 
+    int month;
+    int year;
+    public:
+    Date();
+    Date(int y, int m, int d);
+    void operator=(Date& d);
+    bool isLeapYear();
+    int getYear();
+    int getMonth();
+    int getDay();
+    Date& operator+(int n);
+    Date& operator-(int n);
+    Date& operator+(Date& a);
+    bool operator<(Date& a);
+    bool operator<=(Date& a);
+    bool operator>(Date & a);
+    bool operator>=(Date & a);
+    bool operator==(Date& a);
+    bool operator!=(Date& a);
+    friend ostream& operator<<(ostream& output,Date& d );
+};
