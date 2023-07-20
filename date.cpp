ccc3c7da0df5184a6ac9096b356f1829871137dd
NNoorFatima <50588372+NNoorFatima@users.noreply.github.com>
2023-07-20 09:13:39 +0300

    date.cpp

--- date.cpp
+++ date.cpp
@@ -1,45 +1,81 @@
-//============================================================================
+//===========================================================
+string Date:: toString()
+{
+    // string year;
+    // string month;
+    // string day;
+    // //first find the length of the year
+    // int s=year;int i=0;
+    // while((s/=10) != 0)
+    //     i++;
+    // i++;
+        
+}
+//===========================================================
 Date& Date::operator-(int n)
 {
     if(n<day)
     {
-        day-=n;
+        day= day-n;
     }
     else 
     {
+        n=  n-day;
+        
         if(month!=1)
-            month-=1;
+                month--;
         else
-        {    
+        {
+            year--;
             month=12;
-            year-=1;
-            
         }
-        int days_left=n-day;
-        if(month==2)
+        
+        
+        int month_days;
+        if(month==4||month==6||month==9||month==11)
+            month_days=30;
+        else if(month==2)
         {
             if(isLeapYear()==1)
-            {
-                day=29-days_left;
-            }
+                month_days=29;
             else
-            {
-                day=28-days_left;
-            }
-        }
-        else if(month==8||month==1 || month==3 || month==5 || month==7 || month==10 || month==12)
-        {
-            day=31-days_left;
+                month_days=28;
         }
         else
+            month_days=31;
+          
+        while (n>=month_days)
         {
-            day=30-days_left;
+            n= n-month_days;
+            if(month!=1)
+                month--;
+            else
+            {
+                year--;
+                month=12;
+            }
+            //finding month days again 
+            if(month==4||month==6||month==9||month==11)
+                month_days=30;
+            else if(month==2)
+            {
+                if(isLeapYear()==1)
+                    month_days=29;
+                else
+                    month_days=28;          //eg dates was 2023/5/16 and n= 50   n=19 month=4 so now while conditon is 19>=30
+            }                               //exits loop
+            else
+                month_days=31;
+            
         }
+        day= month_days-n;
+        
+        
     }
-    return *this;
+        return *this;
     
 }
-//============================================================================
+//===========================================================
 Date& Date:: operator+(int n)       
 {
     //jan, mar, may, july, aug, oct, dec have 31 days 
@@ -121,8 +157,7 @@ Date& Date:: operator+(int n)
     return *this;
     
 }
-//============================================================================
-
+//===========================================================
 bool Date :: operator>=(Date& a)
 {
     if(year>=a.year)
@@ -139,8 +174,7 @@ bool Date :: operator>=(Date& a)
     }
     return 0;
 }
-//============================================================================
-
+//===========================================================
 bool Date :: operator>(Date& a)
 {
     if(year>a.year)
@@ -157,8 +191,7 @@ bool Date :: operator>(Date& a)
     }
     return 0;
 }
-//============================================================================
-
+//===========================================================
 bool Date:: operator<=(Date& a)
 {
     if(year<=a.year)
@@ -175,8 +208,7 @@ bool Date:: operator<=(Date& a)
     }
     return 0;
 }
-//============================================================================
-
+//===========================================================
 bool Date:: operator<(Date& a)
 {
     if(year<a.year)
@@ -193,7 +225,7 @@ bool Date:: operator<(Date& a)
     }
     return 0;
 }
-//============================================================================
+//===========================================================
 
 bool Date:: operator!=(Date& a)
 {
@@ -201,7 +233,7 @@ bool Date:: operator!=(Date& a)
         return 0;
     return 1;
 }
-//============================================================================
+//===========================================================
 
 bool Date:: operator==(Date& a)
 {
@@ -209,26 +241,20 @@ bool Date:: operator==(Date& a)
         return 1;
     return 0;
 }
-//============================================================================
-
+//===========================================================
 int Date::getYear()
 {
     return year;
 }
-//============================================================================
-
 int Date::getMonth()
 {
         return month;
 }
-//============================================================================
-
 int Date::getDay()
 {
     return day;
 }
-//============================================================================
-
+//===========================================================
 bool Date:: isLeapYear()
 {
     //leap year is multipleof 4 
@@ -241,8 +267,7 @@ bool Date:: isLeapYear()
     else
         return 0;
 }
-//============================================================================
-
+//===========================================================
 void Date:: operator=(Date& d)
 {
     year=d.year;
@@ -250,31 +275,28 @@ void Date:: operator=(Date& d)
     day=d.day;
     
 }
-//============================================================================
-
+//===========================================================
 Date :: Date()
 {
     day=0;
     month=0;
     year=0;
 }
-//============================================================================
-
+//===========================================================
 Date :: Date(int y, int m ,int d)
 {
     day=d;
     year=y;
-    month=m;  
+    month=m;
+    
 }
-//============================================================================
-
+//===========================================================
 ostream& operator<<(ostream& out, Date& d)
 {
     out<<d.year<<"/"<<d.month<<"/"<<d.day<<endl;
     return out;
 }
-//============================================================================
-
+//===========================================================
 Date& Date:: operator+(Date& a)
 {
     int new_month;
@@ -287,9 +309,10 @@ Date& Date:: operator+(Date& a)
     if((month+a.month)>12)
     {
         new_year+=1;
+        year=new_year;
         new_month=(month+a.month)-12;
         month=new_month;
-      //  cout<<"HEHE "<<new_month<<endl;
+        cout<<"HEHE "<<new_month<<endl;
     }
     else
     {    
@@ -331,7 +354,89 @@ Date& Date:: operator+(Date& a)
             month=new_d.month;
             day=new_d.day;
         }
-    }  
+    }
+    
+    
     return *this;
 }
-//============================================================================
+//===========================================================
+Date& Date:: operator-(Date& a)
+{
+    int new_month;
+    int new_year;
+    int new_day;
+    //finding the new year
+    if(year>a.year)
+    {
+        new_year=year-a.year;
+        year=new_year;
+    }
+    else if(a.year>year)
+    {
+        new_year=a.year-year;
+        year=new_year;
+    }
+    else //if the years are same 
+        new_year=year;
+        
+        
+    //finding the new month
+    if(month>a.month)
+    {
+        new_month=month-a.month;
+        month=new_month;
+    }
+    else if(a.month>month)
+    {
+        new_month=a.month-month;
+        month=new_month;
+    }
+    else//is the months are same 2002/12/1 and 2023/12/1
+    {
+       new_month=month; 
+    }
+
+    //now find the days 
+    if(day>a.day)
+    {
+        new_day=day-a.day;
+    }
+    else if(a.day>day)
+    {
+        new_day=a.day-day;
+    }
+    else 
+        new_day=day;
+    int month_days;
+    
+    //now we checck 
+    if(new_month==4||new_month==6||new_month==9||new_month==11)
+        month_days=30;
+    else if(new_month==2)
+    {
+        if(isLeapYear()==1)
+            month_days=29;
+        else
+            month_days=28;
+    }
+    else
+        month_days=30;
+        
+    if(new_day>month_days)
+    {
+        new_day= new_day - month_days;
+        if(new_month<12)
+            new_month+=1;
+        else
+        {
+            new_month=1;
+            new_year+=1;
+        }
+    }
+    
+    day=new_day;
+    month=new_month;
+    year=new_year;
+    
+    return *this;
+}
